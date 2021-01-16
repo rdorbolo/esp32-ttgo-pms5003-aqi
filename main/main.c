c3113bc77e7d572372e54d6b22eaeab4898b8958
dorbolo <you@example.com>
2021-01-15 20:43:25 -0800

    added second sensor

--- main/main.c
+++ main/main.c
@@ -21,13 +21,17 @@
 
 #include "../../esp32-ttgo-display/main/ttgo.h"
 
-#define UART_RX 27
-#define UART_TX 26
+#define UART_RX1 27
+#define UART_RX2 26
+
+#define UART_TX1 25
+#define UART_TX2 37
 
 extern uint8_t bgRed, bgGreen, bgBlue;
 unsigned test = 0;
+unsigned refreshNow = 1;
 
-struct AirData
+typedef struct AirData_t
 {
     unsigned pm1;
     unsigned pm2_5;
@@ -38,8 +42,16 @@ struct AirData
     unsigned count2_5;
     unsigned count5;
     unsigned count10;
+    int animationCnt;
+    int aqi;
+    int pinNo;
+    int sensorNumber;
+    uart_port_t uart_num;
     int64_t sampleTime;
-} airData;
+    int64_t oldSampleTime;
+} AirData;
+
+AirData airData1, airData2;
 
 int displayMode = 0;
 
@@ -120,29 +132,41 @@ void test1()
 
     //TTGO Logo is top
     char s[30];
+    char s1[30];
+    char s2[30];
     int count = 0;
     int oldColorNumber = -1;
-    int64_t oldSampleTime = 0;
-    int animationCnt = 0;
+
+    airData1.animationCnt = 0;
+    airData2.animationCnt = 0;
+    airData1.oldSampleTime = 0;
+    airData2.oldSampleTime = 0;
+
+    int64_t nextRefresh = 0;
+
+    //if (displayMode == 0)
+    //{
+    //    bgRed = 0x00;
+    //    bgGreen = 0x00;
+    //    bgBlue = 0x00;
+    //    clearScreen(bgRed, bgGreen, bgBlue);
+    //}
 
     while (1)
     {
-
+        unsigned aqi = 33;
+        int x,y;
         if (displayMode == 0)
         {
-            bgRed = 0x00;
-            bgGreen = 0x00;
-            bgBlue = 0x00;
-            clearScreen(bgRed, bgGreen, bgBlue);
-        }
 
-        while (1)
-        {
-
-            if (displayMode == 0)
+            int64_t currentTime = esp_timer_get_time();
+            if (currentTime > nextRefresh)
             {
+                printf("currentTime %lli\n", currentTime);
+                nextRefresh = currentTime + 5000000;
 
-                unsigned aqi = calcAqi(airData.pm2_5);
+                aqi = (airData1.aqi + airData2.aqi) / 2;
+
                 unsigned colorNumber;
 
                 if (aqi < 51)
@@ -188,99 +212,220 @@ void test1()
                     bgBlue = 0x30;
                 }
 
-                gpio_set_intr_type(35, GPIO_PIN_INTR_POSEDGE);
-
-                if (oldColorNumber != colorNumber)
+                if ((oldColorNumber != colorNumber) || refreshNow)
+                {
                     clearScreen(bgRed, bgGreen, bgBlue);
+                    refreshNow = 0;
+                }
                 oldColorNumber = colorNumber;
 
-                if (oldSampleTime != airData.sampleTime)
-                {
+                int x = 40;
+                // int y = 10;
+                const int yMid = 135 / 2;
+                count++;
 
-                    int x = 40;
-                    // int y = 10;
-                    const int yMid = 135 / 2;
-                    count++;
+                snprintf(s, 30, "AQI");
+                displayStr(s, x, yMid - 16, 0xf0, 0xf0, 0xf0, 32);
+                snprintf(s, 30, "%d   ", aqi);
+                displayStr(s, x + 60, yMid - 28, 0xf0, 0xf0, 0xf0, 64);
 
-                    snprintf(s, 30, "AQI");
-                    displayStr(s, x, yMid - 16, 0xf0, 0xf0, 0xf0, 32);
-                    snprintf(s, 30, "%d   ", aqi);
-                    displayStr(s, x + 60, yMid - 28, 0xf0, 0xf0, 0xf0, 64);
+                snprintf(s, 30, "S1:");
+                displayStr(s, 10, 135 - 32, 0, 0, 0, 32);
 
-                    //snprintf(s, 30, "PM2.5");
-                    //displayStr(s, x+90, y,0xf0, 0xf0, 0xf0, 32);
+                snprintf(s, 30, "S2:");
+                displayStr(s, 120, 135 - 32, 0, 0, 0, 32);
 
-                    //snprintf(s, 30, "%d  ", airData.pm2_5 );
-                    //displayStr(s, x+5+90, y,0xf0, 0xf0, 0xf0, 64);
-
-                    //snprintf(s, 30, "pm2.5:  %d   ", airData.pm2_5);
-                    //displayStr(s, x, y,0xf0, 0xf0, 0xf0);
-                    //y = y + 32;
-                    //snprintf(s, 30, "0.3 qty:  %d   ", airData.count_3);
-                    //displayStr(s, x, y,0xf0, 0xf0, 0xf0);
-                    //y = y + 32;
-
-                    const int v = 3818 + (adc1_get_raw(ADC1_CHANNEL_6) * 1000 - 2080 * 1000) / 625;
-                    snprintf(s, 30, "%d.%d V  ", v / 1000, ((v % 1000) + 50) / 100);
-                    //snprintf(s, 30, "%lld   ", airData.sampleTime / 1000000);
-                    displayStr(s, 10, 135 - 32, 0xf0, 0xf0, 0xf0, 32);
-
-                    //fillBox(210, 15, 14, 14, 0xff, 0xff, 0xff);
-
-                    animationCnt = 0;
-                    //printf("1\n");
-                }
-
-                if (animationCnt < 10)
-                {
-
-                    const u_int8_t color = 0xff - animationCnt*0x0f;
-                    fillBox(210, 15, 14, 14, color, color, color);
-                    animationCnt++;
-                }
-
-                oldSampleTime = airData.sampleTime;
+                //const int v = 3818 + (adc1_get_raw(ADC1_CHANNEL_6) * 1000 - 2080 * 1000) / 625;
+                //snprintf(s, 30, "%d.%d V  ", v / 1000, ((v % 1000) + 50) / 100);
+                //displayStr(s, 10, 135 - 32, 0xf0, 0xf0, 0xf0, 32);
             }
-            else
+
+            if (airData1.animationCnt == 0)
+            {
+                snprintf(s1, 30, "%d    ", airData1.aqi);
+                //displayStr(s, 10, 135 - 32, 0xf0, 0xf0, 0xf0, 32);
+            }
+            if (airData2.animationCnt == 0)
+            {
+                snprintf(s2, 30, "%d    ", airData2.aqi);
+                //displayStr(s, 80, 135 - 32, 0xf0, 0xf0, 0xf0, 32);
+            }
+
+            if (airData1.animationCnt < 10)
             {
 
-                uint8_t random;
-                random = (uint8_t)esp_random();
-                bgRed = random;
-                bgGreen = random;
-                bgBlue = random;
-                clearScreen(random, random, random >> 2);
-                vTaskDelay(50 / portTICK_PERIOD_MS);
-
-                //wrCmmd(ST7789_MADCTL);
-                //wrData(0b01101000);
-
-                int x = 20;
-                int y = 20;
-
-                snprintf(s, 30, "This ");
-                x = displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
-                y = y + 32;
-                snprintf(s, 30, "is a ");
-                x = displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
-                y = y + 32;
-                snprintf(s, 30, "Test ");
-                x = displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
-                y = y + 32;
+                const u_int8_t color = 0xff - airData1.animationCnt * 0x0f;
+                //fillBox(210, 15, 14, 14, color, color, color);
+                displayStr(s1, 60, 135 - 32, color, color, color, 32);
+                airData1.animationCnt++;
             }
 
-            //vTaskDelay(1000 / portTICK_PERIOD_MS);
-            //count++;
+            if (airData2.animationCnt < 10)
+            {
 
-            vTaskDelay(50 / portTICK_PERIOD_MS);
+                const u_int8_t color = 0xff - airData2.animationCnt * 0x0f;
+                //fillBox(210, 34, 14, 14, color, color, color);
+                displayStr(s2, 180, 135 - 32, color, color, color, 32);
+                airData2.animationCnt++;
+            }
+
+            airData1.oldSampleTime = airData1.sampleTime;
+        } // displaymode == 1
+
+        else if (displayMode == 1 || displayMode == 2)
+        {
+            AirData *ad;
+            if (displayMode == 1) ad = &airData1;
+            else ad = &airData2;
+
+            if (refreshNow)
+            {
+                clearScreen(bgRed, bgGreen, bgBlue);
+                refreshNow = 0;
+
+                int x = 5;
+                int y = 5;
+
+                snprintf(s, 30, "Sensor %d PM Values",  ad->sensorNumber);
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+                snprintf(s, 30, "PM1 um:");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+                snprintf(s, 30, "PM2.5 um:");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+                snprintf(s, 30, "PM10 um:");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+
+            }
+            int x = 140;
+            int y = 5+32;
+
+            snprintf(s, 30, "%d  ", ad->pm1);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+            snprintf(s, 30, "%d  ", ad->pm2_5);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+            snprintf(s, 30, "%d  ", ad->pm10);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+
+
         }
-    }
+        else if (displayMode == 3 || displayMode == 4)
+        {
+            AirData *ad;
+            if (displayMode == 1) ad = &airData1;
+            else ad = &airData2;
+
+            if (refreshNow)
+            {
+                clearScreen(bgRed, bgGreen, bgBlue);
+                refreshNow = 0;
+
+                int x = 5;
+                int y = 5;
+
+                snprintf(s, 30, "Sensor %d Prtcl Cnts", ad->sensorNumber);
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+                
+                snprintf(s, 30, ".3");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+                snprintf(s, 30, ".5");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+                snprintf(s, 30, "1");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                //y = y + 32;
+
+                x = 120;
+                y = 5 + 32;
+                
+                snprintf(s, 30, "2.5");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+                snprintf(s, 30, "5");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+                snprintf(s, 30, "10");
+                displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+                y = y + 32;
+
+            }
+
+            x = 40;
+            y = 5+32;
+
+            snprintf(s, 30, "%d     ", ad->count_3);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+            snprintf(s, 30, "%d     ", ad->count_5);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+            snprintf(s, 30, "%d     ", ad->count1);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+
+            x = 120+50;
+            y = 5+32;
+            snprintf(s, 30, "%d   ", ad->count2_5);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+            snprintf(s, 30, "%d   ", ad->count5);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+            snprintf(s, 30, "%d   ", ad->count10);
+            displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+
+        }
+        else
+        {
+
+            uint8_t random;
+            random = (uint8_t)esp_random();
+            bgRed = random;
+            bgGreen = random;
+            bgBlue = random;
+            clearScreen(random, random, random >> 2);
+            vTaskDelay(50 / portTICK_PERIOD_MS);
+
+            //wrCmmd(ST7789_MADCTL);
+            //wrData(0b01101000);
+
+            int x = 20;
+            int y = 20;
+
+            snprintf(s, 30, "This ");
+            x = displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+            snprintf(s, 30, "is a ");
+            x = displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+            snprintf(s, 30, "Test ");
+            x = displayStr(s, x, y, 0xff, 0xff, 0xff, 32);
+            y = y + 32;
+        }
+
+        //vTaskDelay(1000 / portTICK_PERIOD_MS);
+        //count++;
+
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+        gpio_set_intr_type(35, GPIO_PIN_INTR_POSEDGE);
+
+    } // end of while(1)
 }
 
 void gpio_int(void *arg)
 {
     gpio_set_intr_type(35, GPIO_PIN_INTR_DISABLE);
-    test = (test + 50) % 500;
+    //test = (test + 50) % 500;
+    displayMode = (displayMode + 1) % 5;
+    refreshNow = 1;
 }
 
 static const int RX_BUF_SIZE = 1024;
@@ -299,10 +444,22 @@ void init(void)
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
         .source_clk = UART_SCLK_APB,
     };
+
+    airData1.pinNo = UART_RX1;
+    airData1.uart_num = UART_NUM_1;
+    airData1.sensorNumber = 1;
+    airData2.pinNo = UART_RX2;
+    airData2.uart_num = UART_NUM_2;
+    airData2.sensorNumber = 2;
+
     // We won't use a buffer for sending data.
-    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
-    uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, UART_TX, UART_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_driver_install(airData1.uart_num, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(airData1.uart_num, &uart_config);
+    uart_set_pin(airData1.uart_num, UART_PIN_NO_CHANGE, airData1.pinNo, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    uart_driver_install(airData2.uart_num, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(airData2.uart_num, &uart_config);
+    uart_set_pin(airData2.uart_num, UART_TX1, airData2.pinNo, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
     gpio_set_direction(35, GPIO_MODE_INPUT);
     gpio_set_intr_type(35, GPIO_INTR_POSEDGE);
@@ -310,19 +467,20 @@ void init(void)
     gpio_isr_handler_add(35, gpio_int, NULL);
 }
 
-static void rx_task(void *arg)
+static void rxTasks(AirData *airData)
 {
     static const char *RX_TASK_TAG = "RX_TASK";
     esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
     uint8_t *data = (uint8_t *)malloc(RX_BUF_SIZE + 1);
     while (1)
     {
-        const int rxBytes = uart_read_bytes(UART_NUM_1, data, 32, 1000 / portTICK_RATE_MS);
+        const int rxBytes = uart_read_bytes(airData->uart_num, data, 32, 1000 / portTICK_RATE_MS);
         if (rxBytes > 0)
         {
             data[rxBytes] = 0;
             //ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
             //ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+            //printf("%d\n", airData->uart_num);
 
             if ((data[0] == 'B') &&
                 (data[1] == 'M') &&
@@ -331,13 +489,17 @@ static void rx_task(void *arg)
                 (data[3] == (uint8_t)28))
             {
 
-                airData.pm1 = (((unsigned)data[4]) << 8) + (unsigned)data[5];
-                airData.pm2_5 = (((unsigned)data[6]) << 8) + (unsigned)data[7];
-                airData.pm10 = (((unsigned)data[8]) << 8) + (unsigned)data[9];
-                airData.count_3 = (((unsigned)data[16]) << 8) + (unsigned)data[17];
+                airData->pm1 = (((unsigned)data[4]) << 8) + (unsigned)data[5];
+                airData->pm2_5 = (((unsigned)data[6]) << 8) + (unsigned)data[7];
+                airData->pm10 = (((unsigned)data[8]) << 8) + (unsigned)data[9];
+                airData->count_3 = (((unsigned)data[16]) << 8) + (unsigned)data[17];
+                airData->count_5 = (((unsigned)data[18]) << 8) + (unsigned)data[19];
+                airData->count1  = (((unsigned)data[20]) << 8) + (unsigned)data[21];
 
                 //airData.count_3 =
-                airData.sampleTime = esp_timer_get_time();
+                airData->sampleTime = esp_timer_get_time();
+                airData->animationCnt = 0;
+                airData->aqi = calcAqi(airData->pm2_5);
             }
 
             //printf();
@@ -347,15 +509,27 @@ static void rx_task(void *arg)
 
         //else printf("no data\n");
     }
-    free(data);
 }
+
+static void rx1_task(void *arg)
+{
+    rxTasks(&airData1);
+}
+
+static void rx2_task(void *arg)
+{
+    rxTasks(&airData2);
+}
+
+
 
 void app_main(void)
 {
 
     initTTGO();
     init();
-    xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
+    xTaskCreate(rx1_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
+    xTaskCreate(rx2_task, "uart_rx_task2", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
     //xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
 
     test1();
